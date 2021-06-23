2e94762f1411b23d21e81c17348c0a555b097ddf
erik-engstrom <eekareeka@yahoo.com>
2021-06-23 11:56:06 -0700

    header/footer styling and read functionality complete minus testing

--- src/pages/WhiskeyShow.js
+++ src/pages/WhiskeyShow.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { Card, CardTitle, Col, CardText } from 'reactstrap'
 
 class WhiskeyShow extends Component {
   render() {
+    console.log(this.props.whiskey);
     return (
       <>
-        <h2>This is the Show Page</h2>
+        <Col sm = "6" className = "show-body">
+          <Card body>
+            <CardTitle>Hi, my name is { this.props.whiskey.name }!</CardTitle>
+            <CardText>I am { this.props.whiskey.age } old. I am from { this.props.whiskey.country }. I taste like: { this.props.whiskey.notes }</CardText>
+          </Card>
+        </Col>
+
       </>
     )
   }
